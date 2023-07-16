51a5824cf8f5eee77959d69518623f8370c4381e
santosh kumar sen <santosh7381070816@gmail.com>
2023-07-16 12:32:42 +0530

    edyoda

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import './App.css';
+
+import { Navbar } from "./components/Navbar";
+import { Content } from "./page/Content";
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar/>
+     <Content/>
+    </div>
+  );
+}
+
+export default App;
